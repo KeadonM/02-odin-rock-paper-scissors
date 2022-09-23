a92da48082c753a2b883835cf95576d185612c36
Keadon Mitchell <mitchell.keadon@outlook.com>
2022-09-23 14:32:23 -0500

    Add printRoundWinner playGame and endGame functions

--- code.js
+++ code.js
@@ -25,17 +25,63 @@ function getComputerChoice() {
   }
 }
 
-function getRoundWinner(playerSelection, computerSelection) {
-  computerSelection = getComputerChoice();
-  playerSelection = getPlayerChoice();
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    printRoundWinner(2, playerSelection, computerSelection);
 
-  if (
+    return 2;
+  } else if (
     (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "scissors" && computerSelection == "paper") ||
     (playerSelection == "paper" && computerSelection == "rock")
   ) {
-    return `You win! ${playerSelection} beats ${computerSelection}!`;
+    printRoundWinner(0, playerSelection, computerSelection);
+    return 0;
   }
 
-  return `You lose... ${computerSelection} beats ${playerSelection}.`;
+  printRoundWinner(1, playerSelection, computerSelection);
+  return 1;
+}
+
+function printRoundWinner(winner, playerSelection, computerSelection) {
+  if (winner === 0) {
+    console.log(
+      `You win this round! ${playerSelection} beats ${computerSelection}!`
+    );
+  } else if (winner === 1) {
+    console.log(
+      `You lose this round... ${computerSelection} beats ${playerSelection}.`
+    );
+  } else {
+    console.log(`It's a tie you both chose ${playerSelection}!`);
+  }
+}
+
+function playGame() {
+  let score = [0, 0];
+  for (i = 0; i < 5; i++) {
+    roundWinner = playRound(getPlayerChoice(), getComputerChoice());
+
+    if (roundWinner === 2) {
+      i--;
+    } else {
+      score[roundWinner]++;
+
+      if (score[0] === 3) {
+        console.log(endGame(0, score));
+        break;
+      } else if (score[1] === 3) {
+        console.log(endGame(1, score));
+        break;
+      }
+    }
+  }
+}
+
+function endGame(gameWinner, score) {
+  if ((gameWinner = 0)) {
+    return `You won ${score[0]} - ${score[1]}, congrats!`;
+  }
+
+  return `You lost ${score[1]} - ${score[0]}, try again!`;
 }
