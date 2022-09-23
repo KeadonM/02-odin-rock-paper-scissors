39f573bd18dc03b43d49e8df0ba3f88e3e9e9828
Keadon Mitchell <mitchell.keadon@outlook.com>
2022-09-23 18:58:39 -0500

    Fix bug when ending game

--- code.js
+++ code.js
@@ -1,86 +1,87 @@
 function getPlayerChoice() {
-  let choice;
+    let choice;
 
-  while (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
-    choice = prompt("Choose rock, paper or scissors.");
-    choice = choice.toLowerCase();
-    choice = choice.replace(/\s+/g, "");
-  }
+    while (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+        choice = prompt("Choose rock, paper or scissors.");
+        choice = choice.toLowerCase().replace(/\s+/g, "");
+    }
 
-  return choice;
+    return choice;
 }
 
 function getComputerChoice() {
-  switch (Math.floor(Math.random() * 9) % 3) {
-    case 0:
-      return "rock";
-      break;
-    case 1:
-      return "paper";
-      break;
-    case 2:
-      return "scissors";
-      break;
-  }
+    switch (Math.floor(Math.random() * 9) % 3) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    printRoundWinner(2, playerSelection, computerSelection);
+    if (playerSelection === computerSelection) {
+        printRoundWinner(2, playerSelection, computerSelection);
 
-    return 2;
-  } else if (
-    (playerSelection == "rock" && computerSelection == "scissors") ||
-    (playerSelection == "scissors" && computerSelection == "paper") ||
-    (playerSelection == "paper" && computerSelection == "rock")
-  ) {
-    printRoundWinner(0, playerSelection, computerSelection);
-    return 0;
-  }
+        return 2;
+    } else if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ) {
+        printRoundWinner(0, playerSelection, computerSelection);
+        return 0;
+    }
 
-  printRoundWinner(1, playerSelection, computerSelection);
-  return 1;
+    printRoundWinner(1, playerSelection, computerSelection);
+    return 1;
 }
 
 function printRoundWinner(winner, playerSelection, computerSelection) {
-  if (winner === 0) {
-    console.log(
-      `You win this round! ${playerSelection} beats ${computerSelection}!`
-    );
-  } else if (winner === 1) {
-    console.log(
-      `You lose this round... ${computerSelection} beats ${playerSelection}.`
-    );
-  } else {
-    console.log(`It's a tie you both chose ${playerSelection}!`);
-  }
+    if (winner === 0) {
+        console.log(
+            `You win this round! ${playerSelection} beats ${computerSelection}!`
+        );
+    } else if (winner === 1) {
+        console.log(
+            `You lose this round... ${computerSelection} beats ${playerSelection}.`
+        );
+    } else {
+        console.log(`It's a tie you both chose ${playerSelection}!`);
+    }
 }
 
 function playGame() {
-  let score = [0, 0];
-  for (i = 0; i < 5; i++) {
-    roundWinner = playRound(getPlayerChoice(), getComputerChoice());
+    let score = [0, 0];
 
-    if (roundWinner === 2) {
-      i--;
-    } else {
-      score[roundWinner]++;
+    for (i = 0; i < 5; i++) {
+        let roundWinner = playRound(getPlayerChoice(), getComputerChoice());
 
-      if (score[0] === 3) {
-        console.log(endGame(0, score));
-        break;
-      } else if (score[1] === 3) {
-        console.log(endGame(1, score));
-        break;
-      }
+        if (roundWinner === 2) {
+            i--;
+        } else {
+            score[roundWinner]++;
+
+            if (score[0] === 3) {
+                console.log(endGame(0, score));
+                break;
+            } else if (score[1] === 3) {
+                console.log(endGame(1, score));
+                break;
+            }
+        }
+        console.log(i);
     }
-  }
 }
 
 function endGame(gameWinner, score) {
-  if ((gameWinner = 0)) {
-    return `You won ${score[0]} - ${score[1]}, congrats!`;
-  }
+    if ((gameWinner === 0)) {
+        return `You won ${score[0]} - ${score[1]}, congrats!`;
+    }
 
-  return `You lost ${score[1]} - ${score[0]}, try again!`;
+    return `You lost ${score[1]} - ${score[0]}, try again!`;
 }
